ab3e6235212c84f858262054c9f322084961c498
manisha <manisha.verma23@gmail.com>
2023-03-28 18:48:40 +0530

    Introduction to Python

---
+++ python_introduction.py
@@ -0,0 +1,6 @@
+#Introduction to Python
+
+print("Python is a very popular programming language")
+print("Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.")
